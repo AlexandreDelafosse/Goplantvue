92fd6250744023434d3125e53e4d1422f7429508
Théodore Cadet <63633363+FiveTTTTT@users.noreply.github.com>
2023-07-28 16:30:22 +0200

    add references to users from posts + add types user and post

--- GoPlantVue/src/components/AllPosts.vue
+++ GoPlantVue/src/components/AllPosts.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
 import {db} from "@/firebase";
 import {onMounted, ref} from "vue";
-import {collection, getDocs} from "firebase/firestore";
+import {collection, getDocs, getDoc, DocumentReference  } from "firebase/firestore";
+import type {Post, User} from "@/types";
 
-const posts = ref([]);
+
+
+const posts = ref<Post[]>([]);
 
 onMounted(async () =>{
   let postsCollection = await getDocs(collection(db, "posts"));
-  postsCollection.forEach((post) => {
-    posts.value.push({...post.data(), id: post.id})
-    console.log(posts.value.length)
-  })
+
+  const getUser = async (creatorRef: DocumentReference ) => {
+    const userSnap = await getDoc(creatorRef);
+    return { ...userSnap.data(), id: userSnap.id };
+  };
+
+  for (const post of postsCollection.docs) {
+    const postData = post.data();
+    const creatorRef = postData.creator;
+    const creator: User = await getUser(creatorRef);
+
+    const newPost: Post = {
+      ...postData,
+      id: post.id,
+      creator: creator,
+    };
+
+    posts.value.push(newPost);
+    console.log(posts.value)
+
+  }
 })
 
 </script>
@@ -19,6 +39,7 @@ onMounted(async () =>{
   <div>
     <div v-for="post in posts" :key="post.id">
       <p>{{post.title}}</p>
+      <p>{{post.creator.name}}</p>
     </div>
   </div>
 
