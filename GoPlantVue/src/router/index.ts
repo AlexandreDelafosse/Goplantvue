2e07b88c6477f71dd4ffd863abe31c83261bb86e
AlexandreDelafosse  git config --global user.email you@example.comgit config --global user.email alexandre.delafosse@edu.devinci.fr <alexandre.delafosse@edu.devinci.fr>
2023-07-28 11:58:56 +0200

    changingroutes

--- GoPlantVue/src/router/index.ts
+++ GoPlantVue/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 import AllUsers from "@/components/AllUsers.vue"
 import AllPosts from "@/components/AllPosts.vue";
 
@@ -9,7 +9,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: MainView
     },
     // {
     //   path: '/user/:id',
---
+++ GoPlantVue/src/views/MainView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type AllPosts from '@/components/AllPosts.vue';
+</script>
+
+<template>
+  <main>
+    <AllPosts />
+  </main>
+</template>
